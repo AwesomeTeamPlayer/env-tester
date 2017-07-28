1adbe1d2608d46531f7dc1ef82addca6c050556b
SimonIsMe <szym1991@gmail.com>
2017-07-28 18:22:56 +0200

    Build env only with projects-service

---
+++ prepareEnv.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cd services/
+#
+#git clone https://github.com/AwesomeTeamPlayer/auth-service.git
+#cd ./auth-service
+#composer install
+#cd ../
+
+git clone https://github.com/AwesomeTeamPlayer/ProjectsService.git
+cd ./ProjectsService
+composer install
+mkdir ./var/
+chmod a+rwx ./var
+mkdir ./var/log
+mkdir ./var/mysql
+mkdir ./var/log/nginx
+cd ../
+
+#git clone https://github.com/AwesomeTeamPlayer/SourceListener.git
+#cd ./SourceListener
+#composer install
+#cd ../
