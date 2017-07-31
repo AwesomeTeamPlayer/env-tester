3610388ca3e21d697089f05b2546c83e5557c801
SimonIsMe <szym1991@gmail.com>
2017-07-31 18:55:14 +0200

    Add auth-service to env

--- prepareEnv.sh
+++ prepareEnv.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 mkdir services
-cd services/
-#
-#git clone https://github.com/AwesomeTeamPlayer/auth-service.git
-#cd ./auth-service
-#composer install
-#cd ../
 
+cd ./services
+git clone https://github.com/AwesomeTeamPlayer/auth-service.git
+cd ./auth-service
+rm ./.travis.yml
+composer install
+mkdir ./var/
+chmod a+rwx ./var
+mkdir ./var/log
+mkdir ./var/mysql
+mkdir ./var/log/nginx
+cd ../../
+
+cd ./services
 git clone https://github.com/AwesomeTeamPlayer/ProjectsService.git
 cd ./ProjectsService
 rm ./.travis.yml
