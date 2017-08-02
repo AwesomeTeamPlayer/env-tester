29f2a12dbb32bc17acdc99270994c37ef91841a4
SimonIsMe <szym1991@gmail.com>
2017-08-02 22:30:09 +0200

    Fix DefaultEndpointTest in projects-service

--- tester/tests/services/ProjectsService/DefaultEndpointTest.php
+++ tester/tests/services/ProjectsService/DefaultEndpointTest.php
@@ -7,7 +7,7 @@ class DefaultEndpointTest extends AbstractEndToEndTest
 	public function test_default_endpoint()
 	{
 		$this->assertEquals(
-			'{"type":"projects-service","config":{"rabbitmq":{"host":"projects-service-rabbitmq","port":5672,"user":"guest","password":"guest","channel":"events"},"mysql":{"host":"projects-service-mysql","port":3306,"user":"root","password":"","database":"testdb"}},"status":{"is_connected":{"MySQL":true,"RabbitMQ":true}}}',
+			'{"type":"projects-service","config":{"rabbitmq":{"host":"events-rabbitmq","port":5672,"user":"guest","password":"guest","channel":"events"},"mysql":{"host":"projects-service-mysql","port":3306,"user":"root","password":"","database":"testdb"}},"status":{"is_connected":{"MySQL":true,"RabbitMQ":true}}}',
 			$this->makeRequest('GET', '/')
 		);
 	}
