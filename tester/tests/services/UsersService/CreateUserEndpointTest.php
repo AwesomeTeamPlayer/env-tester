87f3d2019ece817baa1b8e732432b166c56d7e42
SimonIsMe <szym1991@gmail.com>
2017-08-12 11:42:37 +0200

    Add CreateUserEndpointTest

--- tester/tests/services/UsersService/CreateUserEndpointTest.php
+++ tester/tests/services/UsersService/CreateUserEndpointTest.php
@@ -28,6 +28,58 @@ class CreateUserEndpointTest extends AbstractEndToEndTest
 		);
 	}
 
+	public function test_create_endpoint_with_empty_email()
+	{
+		$this->assertEquals(
+			'{"status":"failed","email":[{"codeId":101,"text":"Given email address is incorrect"}],"name":[{"codeId":104,"text":"Name is required"}],"isActive":[{"codeId":107,"text":"IsActive value is required"}]}',
+			$this->makeRequest('PUT', '/users', '{"email":""}')
+		);
+	}
+
+	public function test_create_endpoint_with_incorrect_email()
+	{
+		$this->assertEquals(
+			'{"status":"failed","email":[{"codeId":101,"text":"Given email address is incorrect"}],"name":[{"codeId":104,"text":"Name is required"}],"isActive":[{"codeId":107,"text":"IsActive value is required"}]}',
+			$this->makeRequest('PUT', '/users', '{"email":"aaa"}')
+		);
+	}
+
+	public function test_create_endpoint_with_too_short_name()
+	{
+		$this->assertEquals(
+			'{"status":"failed","name":[{"codeId":105,"text":"Name is too short (minimal length is 3)"}],"isActive":[{"codeId":107,"text":"IsActive value is required"}]}',
+			$this->makeRequest('PUT', '/users', '{"email":"john@domain.com", "name":"a"}')
+		);
+	}
+
+	public function test_create_endpoint_with_too_long_name()
+	{
+		$this->assertEquals(
+			'{"status":"failed","name":[{"codeId":106,"text":"Name is too long (maximal length is 255)"}],"isActive":[{"codeId":107,"text":"IsActive value is required"}]}',
+			$this->makeRequest('PUT', '/users', '{"email":"john@domain.com", "name":"' . $this->generateString(300) . '"}')
+		);
+	}
+
+	public function test_create_endpoint_with_no_boolean_isActive_value()
+	{
+		$this->assertEquals(
+			'{"status":"failed","isActive":[{"codeId":108,"text":"IsActive value has to be boolean value"}]}',
+			$this->makeRequest('PUT', '/users', '{"email":"john@domain.com", "name":"John", "isActive":"abc123"}')
+		);
+	}
+
+	private function generateString(int $length) : string
+	{
+		$string = '';
+
+		for ($i = 0; $i < $length; $i++)
+		{
+			$string .= 'a';
+		}
+
+		return $string;
+	}
+
 	public function test_create_endpoint_with_correct_json()
 	{
 		$this->assertEquals(
@@ -46,5 +98,21 @@ class CreateUserEndpointTest extends AbstractEndToEndTest
 			'isActive' => true
 		], $events[0]['data']);
 		$this->assertEquals(['name', 'occuredAt', 'data'], array_keys($events[0]));
+	}
+
+	public function test_create_endpoint_when_user_already_exists()
+	{
+		$this->makeRequest('PUT', '/users', '{"email":"a@b.com", "name":"abc", "isActive":true}');
+
+		$this->assertEquals(
+			'{"status":"failed","email":[{"codeId":102,"text":"Given email address already exists"}]}',
+			$this->makeRequest('PUT', '/users', '{"email":"a@b.com", "name":"abc", "isActive":true}')
+		);
+
+		$results = $this->mysqli->query('SELECT * FROM users WHERE email="a@b.com"');
+		$this->assertEquals(1, $results->num_rows);
+
+		$events = $this->getAllStorageEvents();
+		$this->assertCount(1, $events);
 	}
 }
